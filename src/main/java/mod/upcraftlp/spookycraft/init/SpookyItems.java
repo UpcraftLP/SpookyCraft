ed2d77bbfee16c156a3566716a0a83d5cb12272a
UpcraftLP <j-schirra@gmx.de>
2017-10-05 01:08:52 +0200

    added glass string, chocolate bar + candy

--- src/main/java/mod/upcraftlp/spookycraft/init/SpookyItems.java
+++ src/main/java/mod/upcraftlp/spookycraft/init/SpookyItems.java
@@ -12,4 +12,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class SpookyItems {
 
     public static final Item ENCYCLOPEDIA = new ItemEncyclopedia();
+    public static final Item GLASS_STRING = new core.upcraftlp.craftdev.api.item.Item("glass_string");
+    public static final Item CANDY = new core.upcraftlp.craftdev.api.item.Item("candy");
+    public static final Item CHOCOLATE_BAR = new core.upcraftlp.craftdev.api.item.Item("chocolate_bar");
 }
