1be50bf06c7610247b20eabd76d54d8adea84e74
Unknown <j-schirra@gmx.de>
2017-10-06 07:58:06 +0200

    fixed sound events

--- src/main/java/mod/upcraftlp/spookycraft/init/SpookySounds.java
+++ src/main/java/mod/upcraftlp/spookycraft/init/SpookySounds.java
@@ -3,7 +3,11 @@ package mod.upcraftlp.spookycraft.init;
 import core.upcraftlp.craftdev.api.util.ModHelper;
 import mod.upcraftlp.spookycraft.Reference;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import org.apache.commons.codec.language.Soundex;
 
 /**
  * @author UpcraftLP
@@ -11,11 +15,22 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MODID)
 public class SpookySounds {
 
-    public static final SoundEvent SCARECROW_HURT = register("scarecrow_hurt");
-    public static final SoundEvent SCARECROW_AMBIENT = register("scarecrow_ambient");
-    public static final SoundEvent SCARECROW_DEATH = register("scarecrow_death");
+    public static final SoundEvent SCARECROW_HURT = null;
+    public static final SoundEvent SCARECROW_AMBIENT = null;
+    public static final SoundEvent SCARECROW_DEATH = null;
 
-    private static SoundEvent register(String soundName) {
-        return ModHelper.registerSound(soundName, Reference.MODID);
+    @Mod.EventBusSubscriber
+    private static class Registry {
+
+        @SubscribeEvent
+        public static void onRegisterSounds(RegistryEvent.Register<SoundEvent> event) { //register all sounds here
+            register("scarecrow_hurt");
+            register("scarecrow_ambient");
+            register("scarecrow_death");
+        }
+
+        private static SoundEvent register(String soundName) {
+            return ModHelper.registerSound(soundName, Reference.MODID);
+        }
     }
 }
