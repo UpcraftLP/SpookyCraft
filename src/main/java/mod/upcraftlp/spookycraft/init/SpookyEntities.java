7fd324109e897a404489381bcc9bbb84e56fa9fb
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-10-07 15:29:02 -0700

    irrelevant cleanup

--- src/main/java/mod/upcraftlp/spookycraft/init/SpookyEntities.java
+++ src/main/java/mod/upcraftlp/spookycraft/init/SpookyEntities.java
@@ -30,33 +30,47 @@ public class SpookyEntities {
 	private static final int eggSecondary = new Color(0x974F1E).getRGB();
 
 	public static void init() {
-		EntityRegistry.registerModEntity(getEntityResource("ScareCrow"), EntityScareCrow.class, "ScareCrow", 0, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		//FIXME re-enable once finished!
-		//EntityRegistry.registerModEntity(getEntityResource("Mirror"), EntityMirror.class, "Mirror", 1, Main.INSTANCE, 80, 3, false, eggPrimary, egg);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalBat"), EntitySkeletalBat.class, "SkeletalBat", 2, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalChicken"), EntitySkeletalChicken.class, "SkeletalChicken", 3, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalCow"), EntitySkeletalCow.class, "SkeletalCow", 4, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalParrot"), EntitySkeletalParrot.class, "SkeletalParrot", 5, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalPig"), EntitySkeletalPig.class, "SkeletalPig", 6, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalWolf"), EntitySkeletalWolf.class, "SkeletalWolf", 7, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalRabbit"), EntitySkeletalRabbit.class, "SkeletalRabbit", 8, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalSheep"), EntitySkeletalSheep.class, "SkeletalSheep", 9, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
-		EntityRegistry.registerModEntity(getEntityResource("SkeletalOcelot"), EntitySkeletalOcelot.class, "SkeletalOcelot", 10, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("ScareCrow"), EntityScareCrow.class, "ScareCrow", 0,
+				Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		// FIXME re-enable once finished!
+		// EntityRegistry.registerModEntity(getEntityResource("Mirror"),
+		// EntityMirror.class, "Mirror", 1, Main.INSTANCE, 80, 3, false,
+		// eggPrimary, egg);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalBat"), EntitySkeletalBat.class, "SkeletalBat", 2,
+				Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalChicken"), EntitySkeletalChicken.class,
+				"SkeletalChicken", 3, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalCow"), EntitySkeletalCow.class, "SkeletalCow", 4,
+				Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalParrot"), EntitySkeletalParrot.class,
+				"SkeletalParrot", 5, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalPig"), EntitySkeletalPig.class, "SkeletalPig", 6,
+				Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalWolf"), EntitySkeletalWolf.class, "SkeletalWolf", 7,
+				Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalRabbit"), EntitySkeletalRabbit.class,
+				"SkeletalRabbit", 8, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalSheep"), EntitySkeletalSheep.class, "SkeletalSheep",
+				9, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
+		EntityRegistry.registerModEntity(getEntityResource("SkeletalOcelot"), EntitySkeletalOcelot.class,
+				"SkeletalOcelot", 10, Main.INSTANCE, 80, 3, false, eggPrimary, eggSecondary);
 
 		/*
-		 * We want our mob to spawn in biomes. If you
-		 * don't add this then it will not spawn automatically but you can of
-		 * course still make it spawn manually
+		 * We want our mob to spawn in biomes. If you don't add this then it
+		 * will not spawn automatically but you can of course still make it
+		 * spawn manually
 		 */
 		EntityRegistry.addSpawn(EntityScareCrow.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 		EntityRegistry.addSpawn(EntitySkeletalBat.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 		EntityRegistry.addSpawn(EntitySkeletalChicken.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 		EntityRegistry.addSpawn(EntitySkeletalCow.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
-		EntityRegistry.addSpawn(EntitySkeletalParrot.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE);
+		EntityRegistry.addSpawn(EntitySkeletalParrot.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.JUNGLE,
+				Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE);
 		EntityRegistry.addSpawn(EntitySkeletalPig.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 		EntityRegistry.addSpawn(EntitySkeletalWolf.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 		EntityRegistry.addSpawn(EntitySkeletalRabbit.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
-		EntityRegistry.addSpawn(EntitySkeletalOcelot.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE);
+		EntityRegistry.addSpawn(EntitySkeletalOcelot.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.JUNGLE,
+				Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE);
 		EntityRegistry.addSpawn(EntitySkeletalSheep.class, 10, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 
 		/*
@@ -76,10 +90,10 @@ public class SpookyEntities {
 
 		/*
 		 * This is the loot table for our mob
-		 
-		LootTableList.register(EntityScareCrow.LOOT_CROW);
-		LootTableList.register(EntitySkeletal.LOOT_SKELETAL);
-*/
+		 * 
+		 * LootTableList.register(EntityScareCrow.LOOT_CROW);
+		 * LootTableList.register(EntitySkeletal.LOOT_SKELETAL);
+		 */
 	}
 
 	private static ResourceLocation getEntityResource(String entityName) {
