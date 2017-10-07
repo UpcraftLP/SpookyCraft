089e411d6694dc31fdb3632f63ece09e9d604230
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2017-10-07 14:14:55 -0700

    sync candt

--- src/main/java/mod/upcraftlp/spookycraft/init/SpookyItems.java
+++ src/main/java/mod/upcraftlp/spookycraft/init/SpookyItems.java
@@ -1,9 +1,8 @@
 package mod.upcraftlp.spookycraft.init;
 
 import mod.upcraftlp.spookycraft.Reference;
-import mod.upcraftlp.spookycraft.item.ItemBoneMilkBucket;
 import mod.upcraftlp.spookycraft.item.ItemEncyclopedia;
-import net.minecraft.init.Items;
+import mod.upcraftlp.spookycraft.item.ItemSpecialCandy;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -15,7 +14,7 @@ public class SpookyItems {
 
     public static final Item ENCYCLOPEDIA = new ItemEncyclopedia();
     public static final Item GLASS_STRING = new core.upcraftlp.craftdev.api.item.Item("glass_string");
-    public static final Item CANDY_BAG = new core.upcraftlp.craftdev.api.item.Item("candy_bag"); //only obtainable by striking a Skeleton pig with lightning, will give some nice buffs
-    public static final Item CANDY_CORN = new core.upcraftlp.craftdev.api.item.Item("candy_corn");
-    public static final Item CHOCOLATE_BAR = new core.upcraftlp.craftdev.api.item.Item("chocolate_bar");
+    public static final Item CANDY_BAG = new ItemSpecialCandy("candy_bag", 10, 8.0f ,false); //only obtainable by striking a Skeleton pig with lightning, will give some nice buffs
+    public static final Item CANDY_CORN = new core.upcraftlp.craftdev.api.item.ItemFood("candy_corn", 1, 2.0f ,false);
+    public static final Item CHOCOLATE_BAR = new core.upcraftlp.craftdev.api.item.ItemFood("chocolate_bar", 4, 4.0f ,false);
 }
