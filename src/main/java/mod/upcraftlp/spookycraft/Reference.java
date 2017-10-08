0395916352514847c8f59f20e5efbbffcd67a49a
Unknown <j-schirra@gmx.de>
2017-10-08 04:20:53 +0200

    updated metadata

--- src/main/java/mod/upcraftlp/spookycraft/Reference.java
+++ src/main/java/mod/upcraftlp/spookycraft/Reference.java
@@ -15,15 +15,16 @@ public class Reference {
     //MISC INFORMATION
     public static final List<String> authors = Lists.newArrayList(
             "UpcraftLP",
-            "EPIIC_THUNDERCAT"
+            "EPIIC_THUNDERCAT",
+            "Subaraki"
     );
     public static final String MOD_DESCRIPTION = "Happy Halloween 2017";
-    public static final String CREDITS = TextFormatting.GOLD + "(C)" + "2017-" + Year.now().getValue() + " UpcraftLP";
+    public static final String CREDITS = TextFormatting.GOLD + "(C)" + "2017-" + Year.now().getValue() + " UpcraftLP, EPIIC_THUNDERCAT";
 
     //META Information
     public static final String MODNAME = "SpookyCraft";
     public static final String MODID = "spookycraft";
-    public static final String DEPENDENCIES = "required-after:forge@[14.23.0.2489,);required-after:craftdevcore@[2.2.4,)";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.0.2489,);required-after:craftdevcore@[2.2.6,)";
     public static final String UPDATE_JSON = "https://gist.githubusercontent.com/UpcraftLP/c59fa5fa48bf4b424c519936b2d9e037/raw";
     public static final String WEBSITE = "https://minecraft.curseforge.com/projects/spookycraft";
 
